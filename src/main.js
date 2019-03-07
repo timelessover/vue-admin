f52cc33b43d9a422337e3d1b5163ddb278bd91b7
timelessover <shanpaodaxia@qq.com>
2019-03-07 20:36:51 +0800

    '完成登录页面'

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import Element from 'element-ui';
 
+Vue.use(Element);
 Vue.config.productionTip = false
 
 new Vue({
